d50945b16905f5180cd076705b6dcf4b502dac44
Juan M. Pintos <166875657+JuanMPintos@users.noreply.github.com>
2024-07-10 09:57:58 +0000

    fin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronombres = ["the", "our"];
+let adjetivos = ["great", "big"];
+let sustantivos = ["jogger", "racoon"];
+let dominio = [".com", ".net", ".es"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let elementosConcatenados = [];
+for (let i = 0; i < pronombres.length; i++) {
+  for (let x = 0; x < adjetivos.length; x++) {
+    for (let z = 0; z < sustantivos.length; z++) {
+      for (let b = 0; b < dominio.length; b++) {
+        elementosConcatenados.push(
+          pronombres[i] + adjetivos[x] + sustantivos[z] + dominio[b]
+        );
+      }
+    }
+  }
+}
+console.log(elementosConcatenados);
